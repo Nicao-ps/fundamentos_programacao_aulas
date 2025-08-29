063d392e9c961bf12f29f6881f37e5553ce0fbb8
Nicao-ps <nps.guimaraes@gmail.com>
2025-08-29 11:15:29 -0300

    add desafios

---
+++ aula_03/desafio_02.py
@@ -0,0 +1,15 @@
+print('\n'+(89*'='))
+print((18*' ')+'Welcome to the freight cost calculator! (Version 2.0)'+(18*' '))
+print((89*'=')+'\n')
+distance = float(input('Please enter the distance to be covered in km: '))
+print('\n'+'Calculating freight cost... Please wait.')
+if distance > 10.0:
+    print('\n'+'Sorry, we do not deliver to distances over 10 km.')
+elif distance > 5.0:
+    freight_cost = 5.00 + (2.00*(distance - 5.0)) + 5.00
+    print('\n'+f'The freight cost for {distance:.2f} km is R${freight_cost:.2f}.')
+elif distance > 0.0:
+    freight_cost = 5.00 + (1.00 * distance)
+    print('\n'+f'Te freight cost for {distance:.2f} km is R${freight_cost:.2f}.')
+else:
+    print('\n'+'Invalid distance input. Please enter a positive float number.')
